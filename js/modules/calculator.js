0a66e366d7369e992b83cfed07ebefddbc5da7cf
CTZDev <carlosteranzavaleta@hotmail.com>
2021-07-13 17:59:15 -0500

    Calculator App Done!

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -40,52 +40,89 @@ export default function calculator() {
       //Mensajes de error
       contextInputs(resultPos, resultInteger, $txtQuantityPeople, $txtAmountBill);
     }
+
+    //Validación del Porcentaje personalizado
+    if (e.target === $customValue) {
+      operationsCalculator(e.target.value);
+      if ($customValue.value === "") {
+        $amount.textContent = "0.00";
+        $total.textContent = "0.00";
+      }
+    }
+  });
+
+  //Estado inicial del contenido
+  $reset.addEventListener("click", (e) => {
+    $txtAmountBill.value = "";
+    $txtQuantityPeople.value = "";
+    $amount.textContent = "0.00";
+    $total.textContent = "0.00";
+    $customValue.value = "";
+    $customValue.disabled = true;
+    $categoryTip.forEach((tip) => (tip.disabled = true));
+    $reset.disabled = true;
   });
 
   //Optimización del codigo del input - Validaciones , activación de elementos desactivados a activados.
   const contextInputs = (resPos, resContext, context, contextOp) => {
     if (resPos !== true || resContext !== true) {
-      $span.textContent = resPos || resContext;
+      let result;
+      if (resPos !== true) result = resPos;
+      else if (resContext !== true) result = resContext;
+      $span.textContent = result;
       searchInputElement(context).appendChild($span);
-      if ((context.value === "" && contextOp.value === "") || context.value === "" || contextOp.value === "") {
+      if (((context.value === "" || contextOp.value === "") && resPos !== true) || resContext !== true) {
         $categoryTip.forEach((tip) => (tip.disabled = true));
         $customValue.disabled = true;
+        $reset.disabled = true;
       }
     } else {
       $span.textContent = "";
       if (context.value !== "" && contextOp.value !== "") {
         $categoryTip.forEach((tip) => (tip.disabled = false));
         $customValue.disabled = false;
+        $reset.disabled = false;
       }
     }
   };
 
-  //Interacción con los botones (porcentajes)
+  //Interacción con los botones (porcentajes) ,Visualización de resultados
   $categoryTip.forEach((tip) => {
     tip.addEventListener("click", (e) => {
-      const $bill = $txtAmountBill.value,
-        $people = $txtQuantityPeople.value;
-
-      //Id de los Span creados , crear vinculo al crear un span
       let percentageTip = e.target.value.split("%")[0];
-      const productBillWithTip = (percentageTip / 100) * $bill;
-      const amount = (productBillWithTip / $people).toFixed(2);
-      const totalPerson = ((Number($bill) + productBillWithTip) / Number($people)).toFixed(2);
+      operationsCalculator(percentageTip);
+      $customValue.value = "";
     });
   });
+
+  //Optimización de los calculos realizados , Tip Amount and Total
+  const operationsCalculator = (perc) => {
+    //Inputs
+    const $bill = $txtAmountBill.value,
+      $people = $txtQuantityPeople.value;
+
+    //Operations
+    const productBillWithTip = (perc / 100) * $bill;
+    const amount = (productBillWithTip / $people).toFixed(2);
+    const totalPerson = ((Number($bill) + productBillWithTip) / Number($people)).toFixed(2);
+
+    $amount.textContent = amount;
+    $total.textContent = totalPerson;
+  };
 }
 
 //Permite validar el ingreso de numeros positivos
 const validityNumbersPositive = (number = undefined) => {
   if (parseInt(number) === 0) return "Can´'t be Zero";
-  if (!number || number === "") return "Empty field, not (- ó +)";
+  if (!number || number === "") return "Empty field";
   if (Math.sign(number) === -1) return "Only positive numbers";
   return true;
 };
 
 //Permite validar solo enteros (Number of people)
 const validityIntegers = (number = undefined) => {
-  if (!parseInt(number)) return "Not Integer";
+  if (!Number.isInteger(Number(number))) return "Not Integer";
+  if (number.includes(".")) return "Not Integer";
   return true;
 };
 
